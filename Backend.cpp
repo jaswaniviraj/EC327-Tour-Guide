d14b25a18f247e27e58160d861dfb910f43546d9
jaswaniviraj <jaswanivirajv@gmail.com>
2024-06-20 19:24:28 -0400

    Completed Back End w/out Time and Weather Pulling

--- Backend.cpp
+++ Backend.cpp
@@ -23,6 +23,7 @@ int main () {
     ifstream myd;
     myd.open("activities.csv");
     string activity, city, weather, time;
+    string choice_city,choice_time,choice_weather,suggested_activity;
     vector <Info> v;
     while (true) {
         myd >> activity >> city >> weather >> time; 
@@ -30,27 +31,51 @@ int main () {
         v.push_back(Info(activity, city, weather, time));
     }
     myd.close();
-    for (auto e: v){
+    /*for (auto e: v){
     cout << e.a << "\n";
-    }
+    }*/
     
-    //GUI SFML @YOUSEF @SOUD
-    cout<<"Enter time or choose the current time: \n";
-    cout<<"Type: Morning Afternoon Evening Night\n";
-    /*YOUSEF SOUD, this needs to be two boxes they click on the screen
-    for the purpose rn the input will manual*/ 
-    string choice_time;
-    cin >> choice_time;
-    if (choice_time == "Morning") {choice_time = 6;}
-    else if (choice_time == "Afternoon") {choice_time = 7;}
-    else if (choice_time == "Evening") {choice_time = 8;}
-    else if (choice_time == "Night") {choice_time = 9;}
+    //MAIN INPUTS
+    cout<<"Select a City: (Jakarta, Bali, Athens, Mykonos) \n";
+    cin >> choice_city;
+    cout<<"Input the time (24 Hr Clock but just the hour): \n";
+    int time_entry;
+    cin >> time_entry;
+        /*YOUSEF SOUD, this needs to be two boxes they click on the screen
+        for the purpose rn the input will manual*/
+        if (time_entry<12){choice_time = '6';}
+        else if (time_entry >=12 && time_entry <17) {choice_time = '7';}
+        else if (time_entry >=17 && time_entry <21) {choice_time = '8';}
+        else if (time_entry >=21 && time_entry <24) {choice_time = '9';}
+    //cout << choice_time;
 
+    cout<<"Input the weather: \n";
+    float weather_entry;
+    cin >> weather_entry;
+        /*YOUSEF SOUD, this needs to be two boxes they click on the screen
+        for the purpose rn the input will manual*/
+        if (weather_entry>29){choice_weather = '1';}
+        else if (weather_entry <=29 && weather_entry >18) {choice_weather = '2';}
+        else if (weather_entry <=18 && weather_entry >0) {choice_weather = '3';}
+        else if (weather_entry <=0) {choice_weather = '4';}
+    //cout << choice_weather;
+ 
+    int checker = 0;
     for (auto e: v){
-        if (e.t.find('6') != std::string::npos){
-            cout << e.a << "\n";
+        if (e.c.find(choice_city) != std::string::npos){
+            if (e.t.find(choice_time) != std::string::npos){
+                 if (e.w.find(choice_weather) != std::string::npos){
+                        suggested_activity = e.a;
+                    for (int i = 0;i<suggested_activity.size();i++){
+                        if (suggested_activity[i] == '_'){
+                            suggested_activity[i] = ' ';
+                        }
+                    }
+                    cout << suggested_activity << "\n";//ADD A IF NOTHING FOUND
+                    checker = 1;
+                 }
+            }
         }
-    
     }
-
+    if (checker == 0) {cout<<"We are sorry, we don't have any recomendations but we are working to get more soon! ";}
 }
